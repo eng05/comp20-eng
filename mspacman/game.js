56450e28f0a98eec383ee3ea8aa87e0eaf040bd5
Elwin <elwin.ng@tufts.edu>
2015-03-02 20:45:43 -0500

    Added code for game.js

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,10 @@
-// Replace this with your code...
+function init() {
+canvas = document.getElementById('game_canvas');
+ctx = canvas.getContext('2d');
+var img = new Image();
+img.src = "pacman10-hp-sprite.png";
+img.addEventListener("load", function() {
+ctx.drawImage(img, 320, 1, 465, 136, 0, 0, 465, 136);
+ctx.drawImage(img, 82, 23, 15, 15, 38, 31, 15, 15);
+}, false);
+}
