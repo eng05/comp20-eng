53a432e1e75b4eefeb6833e8150030d5bdf9bc37
Elwin <elwin.ng@tufts.edu>
2015-03-09 15:40:36 -0400

    Updated lab.js file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,22 @@
-// Your JavaScript goes here...
+function parse() {
+	request = new XMLHttpRequest();
+	request.onreadystatechange = parseData;
+	request.open ("GET", "data.json", true);
+	request.send();
+}
+
+function parseData() {
+	if (request.readystate == 4 && request.status == 200) {
+		messagesDiv = document.getElementById("messages");
+		converted = JSON.parse(request.responseText);
+		for (i=0; i<converted.length; i++) {
+			messagesDiv.innerHTML += "<p>" + converted[i]['content']
+		}
+	}
+	else if (request.readystate == 4 && request.status != 200) {
+		alert("Hacked");
+	}
+	else {
+		console.log("Not done yet...");
+	}
+}
