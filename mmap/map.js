f8fa78f4d5fc05a1b4d69488e0f375280fd40c80
Elwin <elwin.ng@tufts.edu>
2015-03-12 01:20:37 -0400

    Started map.js file and wrote init and getMyLocation functions

---
+++ mmap/map.js
@@ -0,0 +1,32 @@
+var myLat = 0;
+var myLng = 0;
+var request = new XMLHttpRequest();
+var me = new google.maps.LatLng(myLat, myLng);
+var myOptions = {
+	zoom: 13, 
+	center: me,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map;
+var marker;
+var infowindow = new google.maps.InfoWindow();
+var places;
+
+function init() {
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	getMyLocation();
+}
+
+function getMyLocation() {
+	if (navigator.geolocation) { 
+		navigator.geolocation.getCurrentPosition(function(position) {
+			myLat = position.coords.latitude;
+			myLng = position.coords.longitude;
+			renderMap();
+		});
+	}
+	else {
+		alert("Geolocation is not supported by web browser.");
+	}
+}
+
